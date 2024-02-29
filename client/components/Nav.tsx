a4ea1fd662b6db3879bd2c94c1ee6737a10e2972
Kevin Klochan <klochankevin@gmail.com>
2024-02-29 16:02:59 -0600

    splash page styling

--- client/components/Nav.tsx
+++ client/components/Nav.tsx
@@ -1,36 +1,38 @@
-import "../App.css";
-import logo from "../assets/github-logo.png";
-import dashQL_Logo from "../assets/dashQL_Logo.png";
+import '../App.css';
+import logo from '../assets/github-logo.png';
+import dashQL_Logo from '../assets/dashQL_Logo.png';
 
 interface currentPageProps {
-  currentPage: string
+  currentPage: string;
 }
 
-export default function Nav({currentPage}: currentPageProps) {
+export default function Nav({ currentPage }: currentPageProps) {
   return (
     <nav>
-      <div id="left-nav">
-        <a href="/">
-          <img src={dashQL_Logo} alt="dashQL logo"/>
+      <div id='left-nav'>
+        <a href='/'>
+          <img src={dashQL_Logo} alt='dashQL logo' />
         </a>
       </div>
-      <div id="right-nav">
-        <a href="/" id={currentPage === "Home" ? "pageStyle" : ""}>
+      <div id='right-nav'>
+        <a href='/' id={currentPage === 'Home' ? 'pageStyle' : ''}>
           Home
         </a>
-        <a href="/demo" id={currentPage == "Demo" ? "pageStyle" : ""}>
+        <a href='/demo' id={currentPage == 'Demo' ? 'pageStyle' : ''}>
           Demo
         </a>
-        <a href="/docs" id={currentPage == "Docs" ? "pageStyle" : ""}>
+        {/* <a href="/docs" id={currentPage == "Docs" ? "pageStyle" : ""}>
           Docs
-        </a>
+        </a> */}
         <div>
-          <a href="https://github.com/orgs/oslabs-beta/teams/dashql" target="_blank" >
-            <img id="githubLogo" src={logo} alt="github logo"/>
+          <a
+            href='https://github.com/orgs/oslabs-beta/teams/dashql'
+            target='_blank'
+          >
+            <img id='githubLogo' src={logo} alt='github logo' />
           </a>
         </div>
       </div>
     </nav>
-
   );
 }
