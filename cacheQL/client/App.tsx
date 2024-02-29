f80fa0207318f77587d68246627d370f0810170f
droobun <avwilliams1995@gmail.com>
2024-02-28 22:52:46 -0500

    frontend

---
+++ cacheQL/client/App.tsx
@@ -0,0 +1,28 @@
+import {  useState } from "react";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Nav from "./components/Nav";
+import Demo from "./components/Demo";
+
+
+function App() {
+  const [currentPage, setPage] = useState<string>("Home");
+
+  function changePage(page: string): void {
+    setPage(page);
+  }
+
+  return (
+    <>
+      <Nav currentPage={currentPage} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="demo" element={<Demo changePage={changePage} />} />
+        {/* <Route path="/docs" element={<Docs changePage={changePage} />} /> */}
+      </Routes>
+    </>
+  );
+}
+
+export default App;
